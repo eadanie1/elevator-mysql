51a4dca13e19025fdb92642dba299441dd2ff45f
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-02-29 20:59:36 +0200

    minor changes to app.js

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 import express from 'express';
-import mongoose, { mongo } from 'mongoose';
+import mysql from 'mysql';
+// import mongoose, { mongo } from 'mongoose';
 import { getRoutes } from './src/scripts/get-elevator.js';
 import { putRoutes } from './src/scripts/put-elevator.js';
 import { callElevatorAPI, callElevatorRouteHandler } from './src/scripts/post-elevator.js';
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/elevators')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Unable to connect to MongoDB', err));
 
-export const elevatorSchema = new mongoose.Schema({
-  id: Number,
-  currentFloor: Number,
-  status: String,
-  destinationFloor: Number,
-});
 
-export const Elevator = mongoose.model('Elevator', elevatorSchema);
+// mongoose.connect('mongodb://localhost/elevators')
+//   .then(() => console.log('Connected to MongoDB...'))
+//   .catch(err => console.error('Unable to connect to MongoDB', err));
 
-async function createElevator(id) {
-  const elevator = new Elevator({
-    id: id,
-    currentFloor: 0,
-    status: 'idle',
-    destinationFloor: 0
-  }) 
-  const result = await elevator.save();
-  console.log(result);
-}
+// export const elevatorSchema = new mongoose.Schema({
+//   id: Number,
+//   currentFloor: Number,
+//   status: String,
+//   destinationFloor: Number,
+// });
+
+// export const Elevator = mongoose.model('Elevator', elevatorSchema);
+
+// async function createElevator(id) {
+//   const elevator = new Elevator({
+//     id: id,
+//     currentFloor: 0,
+//     status: 'idle',
+//     destinationFloor: 0
+//   }) 
+//   const result = await elevator.save();
+//   console.log(result);
+// }
 // createElevator(1);
 // createElevator(2);
 // createElevator(3);
