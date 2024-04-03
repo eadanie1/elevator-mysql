eccce0d48eec66870a1f64090fe5a3fc65df749f
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-04-03 13:27:07 +0200

    implemented function to mimic movement of elevators

--- frontend/elevator-react/src/components/ElevatorStatus.tsx
+++ frontend/elevator-react/src/components/ElevatorStatus.tsx
@@ -1,43 +1,118 @@
 import { Elevator } from "../types/types";
+import styles from "../styles/elevatorStatus.module.css";
 
 interface ESProps {
   elevators: Elevator[];
+  floor1: number;
+  floor2: number;
+  floor3: number;
 }
 
-const ElevatorStatus = ({ elevators }: ESProps) => {
+const ElevatorStatus = ({ elevators, floor1, floor2, floor3 }: ESProps) => {
+  console.log(elevators);
+
   return (
-    <>
-      <h1>Status of elevators</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Current floor</th>
-            <th>Status</th>
-            <th>Destination floor</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {elevators.map((e) => (
-            <tr key={e.id}>
-              <td>{e.id}</td>
-              <td>{e.currentFloor}</td>
-              <td>{e.status}</td>
-              <td>{e.destinationFloor}</td>
-              <td>
-                <button
-                  onClick={() => console.log("hi")}
-                  className="btn btn-outline-danger"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div className="d-flex">
+      <div
+        className={`${styles.elevator} card`}
+        style={{
+          position: "fixed",
+          bottom: `${floor1 * 50 + 10}px`,
+          left: "22%",
+        }}
+      >
+        <img
+          src={`/src/images/elevator-image.jpg`}
+          className="card-img-top"
+          style={{ width: "170px" }}
+          alt={`Elevator image`}
+        />
+        <div className={`card-body d-flex flex-column justify-content-between`}>
+          <h5 className="card-title">Elevator {elevators[0]?.id}</h5>
+          <p>Current: {elevators[0]?.currentFloor}</p>
+          <p>Status: {elevators[0]?.status}</p>
+          <p>Destination: {elevators[0]?.destinationFloor}</p>
+          <div className="d-flex justify-content-between mt-2">
+            <form action="">
+              <label htmlFor="" className="form-label"></label>
+              <select name="" id="" className="form-select"></select>
+            </form>
+            {/* <button key={`swap-${e.id}`} className={` btn`}>
+                      Swap
+                    </button>
+                    <button key={`delete-${e.id}`} className={` btn`}>
+                      Delete
+                    </button> */}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${styles.elevator} card`}
+        style={{
+          position: "fixed",
+          bottom: `${floor2 * 50 + 10}px`,
+          left: "44%",
+        }}
+      >
+        <img
+          src={`/src/images/elevator-image.jpg`}
+          className="card-img-top"
+          style={{ width: "170px" }}
+          alt={`Elevator image`}
+        />
+        <div className={`card-body d-flex flex-column justify-content-between`}>
+          <h5 className="card-title">Elevator {elevators[1]?.id}</h5>
+          <p>Current: {elevators[1]?.currentFloor}</p>
+          <p>Status: {elevators[1]?.status}</p>
+          <p>Destination: {elevators[1]?.destinationFloor}</p>
+          <div className="d-flex justify-content-between mt-2">
+            <form action="">
+              <label htmlFor="" className="form-label"></label>
+              <select name="" id="" className="form-select"></select>
+            </form>
+            {/* <button key={`swap-${e.id}`} className={` btn`}>
+                      Swap
+                    </button>
+                    <button key={`delete-${e.id}`} className={` btn`}>
+                      Delete
+                    </button> */}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${styles.elevator} card`}
+        style={{
+          position: "fixed",
+          bottom: `${floor3 * 50 + 10}px`,
+          left: "66%",
+        }}
+      >
+        <img
+          src={`/src/images/elevator-image.jpg`}
+          className="card-img-top"
+          style={{ width: "170px" }}
+          alt={`Elevator image`}
+        />
+        <div className={`card-body d-flex flex-column justify-content-between`}>
+          <h5 className="card-title">Elevator {elevators[2]?.id}</h5>
+          <p>Current: {elevators[2]?.currentFloor}</p>
+          <p>Status: {elevators[2]?.status}</p>
+          <p>Destination: {elevators[2]?.destinationFloor}</p>
+          <div className="d-flex justify-content-between mt-2">
+            <form action="">
+              <label htmlFor="" className="form-label"></label>
+              <select name="" id="" className="form-select"></select>
+            </form>
+            {/* <button key={`swap-${e.id}`} className={` btn`}>
+                      Swap
+                    </button>
+                    <button key={`delete-${e.id}`} className={` btn`}>
+                      Delete
+                    </button> */}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
