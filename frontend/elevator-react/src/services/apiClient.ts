cc9ec63d36fc127d1f56db9f569a492a0d93bc99
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-06-09 00:38:19 +0200

    Edit URL to backend again to include /api/elevators

--- frontend/elevator-react/src/services/apiClient.ts
+++ frontend/elevator-react/src/services/apiClient.ts
@@ -1,7 +1,8 @@
 import axios, { CanceledError } from "axios";
 
 export default axios.create({
-  baseURL: 'https://elevator-mysql-production.up.railway.app'
+  // baseURL: 'http://localhost:3000/api/elevators'
+  baseURL: 'https://elevator-mysql-production.up.railway.app/api/elevators'
 });
 
 
